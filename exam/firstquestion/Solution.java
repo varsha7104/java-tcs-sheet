49ecabd0eb3c286507bd683ee76ddab75000e414
varsha7104 <130915408+varsha7104@users.noreply.github.com>
2026-07-13 16:32:13 +0530

    Update Solution.java

--- exam/firstquestion/Solution.java
+++ exam/firstquestion/Solution.java
@@ -1,18 +1,12 @@
 import java.util.*;
 
 public class Solution {
-
-   
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         int a = sc.nextInt(); // First term
         int b = sc.nextInt(); // Number of terms
         int c = sc.nextInt(); // Common ratio
-
-        generateGP(a, b, c);
  if (b <= 0) {
             System.out.println("It is not possible");
             return;
